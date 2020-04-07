e0ee9a296abbf33d84298af5f2a81e49d3538ac8
BennoMP <61586142+BennoMP@users.noreply.github.com>
2020-04-07 19:48:00 +0200

    Icon name edit

---
+++ Panda Bot.py
@@ -0,0 +1,48 @@
+import time
+import discord
+
+TOKEN = 'Njk1OTY3NDc4NTg0NTczOTg0.Xoh5lg.8PjM7J17nNZVOpD0w62eoKAwqIM'
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == 'bot!hi':
+        msg = 'Hello there! :D'.format(message)
+        await message.channel.send(msg)
+
+    if message.content == 'bot!joke':
+            msg = 'Hah. How about NO'.format(message)
+            await message.channel.send(msg)
+
+    if message.content == 'bot!help':
+        msg = '```Panda Bot Command List:```'
+        await message.channel.send(msg)
+        msg = '```bot!hi   : Greets you```'
+        await message.channel.send(msg)
+        msg = '```bot!joke : Tells a joke```'
+        await message.channel.send(msg)
+        msg = '```bot!help : Shows this message```'
+        await message.channel.send(msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('Bot is online now')
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="bot!help"))
+
+client.run(TOKEN)
+
+#def action1():
+#    while True:
+#        print('!d bump')
+#        time.sleep(7200)
+
+#input = input()
+#if input == 'bot!start':
+#    action1()
